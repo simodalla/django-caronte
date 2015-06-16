e47175849605924c26441c3a3d6d94f4340b9df7
Simone Dalla <simodalla@gmail.com>
2015-06-16 17:40:02 +0200

    fix caronte app

--- caronte/urls.py
+++ caronte/urls.py
@@ -4,13 +4,7 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-
-urlpatterns = patterns(
-    '',
-    url(r'^login/$',  views.AccountsLoginRedirectView.as_view()),
-    url(r'^logout/$', views.AccountsLogoutRedirectView.as_view()),
-    url(r'^password/change/$', views.AccountsPasswordChangeView.as_view()),
-    url(r'^inactive/$', views.AccountsInactiveRedirectView.as_view()),
+urlpatterns = [
     url(r'^unauthorizedlogin/$', views.UnauthorizedLogin.as_view(),
-        name='unauthorized_login'),
-)
+        name='unauthorized_login')
+]
